0a7d4d79e157e217474ccaeee7534084a085ee71
LuddeW <ludwig.wiktorsson@gmail.com>
2020-01-15 20:42:00 +0100

    Created BaseInput component

---
+++ client/src/components/_inputs/BaseInput/BaseInput.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+interface BaseInputProps {
+  name: string
+  type: string
+  value: string
+  onChange: (value: string, event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export default function BaseInput({
+  type,
+  name,
+  value,
+  onChange
+}: BaseInputProps) {
+  function onElementChange(event: React.ChangeEvent<HTMLInputElement>) {
+    if (onChange) {
+      onChange(event.target.value, event)
+    }
+  }
+
+  return (
+    <input type={type} name={name} value={value} onChange={onElementChange} />
+  )
+}
