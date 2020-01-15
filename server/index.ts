36a6246efa38fcb49e71d88625dfd831caa6177b
Hektor Wallin <hektorw@gmail.com>
2020-01-15 20:10:43 +0100

    Add heroku build

--- server/index.ts
+++ server/index.ts
@@ -2,4 +2,8 @@ import express from 'express'
 
 const app = express()
 
+app.get('/hello-world', (request, response) => {
+  response.send('Hello world')
+})
+
 app.listen(process.env.PORT || 4002, () => {})
