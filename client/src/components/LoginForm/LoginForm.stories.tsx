faad002a6777be85e02cde006a3a79603af0b586
LuddeW <ludwig.wiktorsson@gmail.com>
2020-01-19 16:44:12 +0100

    Created LoginForm component

---
+++ client/src/components/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import LoginForm from './LoginForm'
+
+export default {
+  component: LoginForm,
+  title: 'components/LoginForm'
+}
+
+export function question() {
+  return <LoginForm />
+}
