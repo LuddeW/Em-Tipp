bcb0d35f4a4584b6576409c82469fc682042ed5c
LuddeW <ludwig.wiktorsson@gmail.com>
2020-01-30 20:01:21 +0100

    Created the ResultDecider component

---
+++ client/src/components/ResultDecider/ResultDecider.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import ResultDecider from './ResultDecider'
+
+export default {
+  component: ResultDecider,
+  title: 'components/ResultDecider'
+}
+
+export function resultDeciderComponent() {
+  return <ResultDecider />
+}
---
+++ client/src/components/ResultDecider/ResultDecider.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+import Checkbox from '../Checkbox/Checkbox'
+
+export default function ResultDecider() {
+  const [oneChecked, setOneChecked] = useState(false)
+  const [xChecked, setXChecked] = useState(false)
+  const [twoChecked, setTwoChecked] = useState(false)
+
+  return (
+    <div>
+      <Checkbox
+        title="1"
+        name="1"
+        checked={oneChecked}
+        onChange={setOneChecked}
+      />
+      <Checkbox title="X" name="X" checked={xChecked} onChange={setXChecked} />
+      <Checkbox
+        title="2"
+        name="2"
+        checked={twoChecked}
+        onChange={setTwoChecked}
+      />
+    </div>
+  )
+}
+
+function setCheckboxes(event) {
+  return ''
+}
