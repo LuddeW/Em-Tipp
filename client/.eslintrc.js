860b486b1072408456d39b8aa34c4cb3309e0592
Hektor Wallin <hektorw@gmail.com>
2020-02-04 17:44:19 +0100

    Start splitting up workspaces, untested

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,7 +1,32 @@
+const baseConfig = require('@em-tipp/tools/.eslintrc')
+
 module.exports = {
-  extends: '../.eslintrc.js',
+  ...baseConfig,
+
+  extends: baseConfig.extends.concat([
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended'
+  ]),
+
+  plugins: baseConfig.plugins.concat(['react', 'react-hooks']),
+
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+
+  rules: {
+    ...baseConfig.rules,
+
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  },
 
   globals: {
+    process: 'readonly',
     __DEV__: 'readonly'
   }
 }
---
+++ tools/.eslintrc.js
@@ -0,0 +1,55 @@
+const prettierConfig = require('./prettier.config')
+
+module.exports = {
+  root: true,
+
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+
+  env: {
+    es6: true
+  },
+
+  extends: [
+    'standard',
+
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+
+    'prettier',
+    'prettier/@typescript-eslint'
+  ],
+
+  plugins: ['@typescript-eslint', 'prettier'],
+
+  rules: {
+    'no-console': 'error',
+    'no-empty-pattern': 'off',
+
+    'prettier/prettier': ['error', prettierConfig],
+
+    'no-unused-expressions': 'off',
+    '@typescript-eslint/no-unused-expressions': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index'
+        ]
+      }
+    ]
+  }
+}
