e4f31a1ca2a70b6341bf25f015cbda64bc7b07e9
Fredrik Skoog <feffmaster@gmail.com>
2020-03-07 21:03:44 +0100

    Added update function to user query

--- server/db/models/users.query.ts
+++ server/db/models/users.query.ts
@@ -1,22 +1,38 @@
-const { get } = require('../postgres.ts')
+const { get, update } = require('../postgres.ts')
 
-export function authenticateUser(username: string, pin: string) {
+export async function authenticateUser(username: string, pin: string) {
   if (username && pin) {
     const query =
       'SELECT id, username, displayname FROM USERS WHERE USERNAME=$1 AND PIN=$2'
     const values = [username, pin]
 
-    return get(query, values).then((result: any) => console.log(result))
+    return get(query, values).then((result: any) => result)
   }
   return null
 }
 
-export function getUserById(id: string) {
+export async function getUserById(id: string) {
   if (id) {
     const query = 'SELECT id, username, displayname FROM USERS WHERE id=$1'
     const values = [id]
 
-    return get(query, values).then((result: any) => console.log(result))
+    return get(query, values).then((result: any) => result)
   }
+  return null
+}
+
+export async function updateDisplayName(id: string, displayname: string) {
+  if (id && displayname) {
+    const query = 'UPDATE USERS SET displayname = $1 WHERE id=$2'
+    const values = [displayname, id]
+
+    const updated = await update(query, values).then((result: any) =>
+      Boolean(result)
+    )
+    const user = await getUserById(id)
+
+    return { user, updated }
+  }
+
   return null
 }
